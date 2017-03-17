60756506f785e60de7e6010940cef3c872db043a
Simonas Kareiva <simonas@5grupe.lt>
2017-03-17 23:28:57 +0200

    Initial LHFA commit

---
+++ app/src/main/java/com/lt/lrmd/lhfa/MainActivity.java
@@ -0,0 +1,257 @@
+package com.lt.lrmd.lhfa;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.kml.KmlDocument;
+import org.osmdroid.bonuspack.kml.Style;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.FolderOverlay;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private static String kmz_url = "http://rk.vdu.lt/phocadownload/Google_Earth/lhfa-updated.kmz";
+    public static KmlDocument mKmlDocument; //made static to pass between activities
+    protected FolderOverlay mKmlOverlay; //root container of overlays from KML reading
+    protected MapView mapView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Pranešk apie ekspediciją!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(false);
+        mapView.setMultiTouchControls(true);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+
+        Drawable marker = getResources().getDrawable(android.R.drawable.star_big_on);
+        int markerWidth = marker.getIntrinsicWidth();
+        int markerHeight = marker.getIntrinsicHeight();
+        marker.setBounds(0, markerHeight, markerWidth, 0);
+
+        GeoPoint startPoint = new GeoPoint(54.9, 23.85);
+        IMapController mapController;
+        mapController = mapView.getController();
+        mapController.setZoom(10);
+        mapController.setCenter(startPoint);
+
+        /*Marker startMarker = new Marker(mapView);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        mapView.getOverlays().add(startMarker);
+
+        startMarker.setIcon(getResources().getDrawable(R.drawable.btn_moreinfo));
+        startMarker.setTitle("Kaunas");*/
+
+        String url = "http://rk.vdu.lt/phocadownload/Google_Earth/lhfa-updated.kmz";
+        mKmlDocument = new KmlDocument();
+        mKmlOverlay = null;
+
+        putKMLAsync(kmz_url);
+
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /**
+     *  https://g.co/AppIndexing/AndroidStudio
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+    private class putKML extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... url) {
+            boolean ok = false;
+            URL urlobj;
+            try {
+                urlobj = new URL(url[0]);
+                URLConnection connection = urlobj.openConnection();
+                InputStream in = connection.getInputStream();
+                File kmzfile = new File(getApplicationContext().getFilesDir().getPath() + "/lhfa.kmz");
+                FileOutputStream fos = new FileOutputStream(kmzfile);
+                byte[] buf = new byte[512];
+                while (true) {
+                    int len = in.read(buf);
+                    if (len == -1) {
+                        break;
+                    }
+                    fos.write(buf, 0, len);
+                }
+                in.close();
+                fos.flush();
+                fos.close();
+
+                ok = mKmlDocument.parseKMZFile(kmzfile);
+
+            }
+            catch(MalformedURLException e){
+                System.out.println("The url is not well formed: " + e.getMessage());
+            }
+            catch (IOException f){
+                System.out.println("IO error:" + f.getMessage());
+            }
+
+            return ok;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean ok) {
+            super.onPostExecute(ok);
+            updateKML();
+        }
+    }
+    private void putKMLAsync(String url) {
+        new putKML().execute(url);
+    }
+
+
+    private void updateKML(){
+        if (mKmlOverlay != null){
+            mKmlOverlay.closeAllInfoWindows();
+            mapView.getOverlays().remove(mKmlOverlay);
+        }
+        Drawable defaultMarker = getResources().getDrawable(R.drawable.marker_default);
+        Bitmap defaultBitmap = ((BitmapDrawable)defaultMarker).getBitmap();
+        Style defaultStyle = new Style(defaultBitmap, 0x901010AA, 3.0f, 0x20AA1010);
+
+        mKmlOverlay = (FolderOverlay) mKmlDocument.mKmlRoot.buildOverlay(mapView, defaultStyle, null, mKmlDocument);
+        mapView.getOverlays().add(mKmlOverlay);
+        mapView.invalidate();
+    }
+
+}
