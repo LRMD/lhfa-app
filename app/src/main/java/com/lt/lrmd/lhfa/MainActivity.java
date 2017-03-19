0711000f865c59a20ea64775043052a2d4f14b45
Simonas Kareiva <simonas@5grupe.lt>
2017-03-19 18:48:29 +0200

    Prevent multiple overlays when asking for permissions

--- app/src/main/java/com/lt/lrmd/lhfa/MainActivity.java
+++ app/src/main/java/com/lt/lrmd/lhfa/MainActivity.java
@@ -301,21 +301,16 @@ public class MainActivity extends AppCompatActivity
 
     private void checkPermissions() {
         List<String> permissions = new ArrayList<String>();
-        String message = "OSMDroid permissions:";
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
-            message += "\nStorage access to store map tiles.";
         }
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
-            message += "\nLocation to show user location.";
         }
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-            message += "\nLocation to show user location.";
         }
         if (!permissions.isEmpty()) {
-            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
             String[] params = permissions.toArray(new String[permissions.size()]);
             requestPermissions(params, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
         } // else: We already have permissions, so handle as normal
